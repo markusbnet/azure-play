33780603624b1d359bfb850ea80eadef1aeacc47
markusbnet <>
2021-02-21 16:24:40 +0000

    querying security center with python sdk

---
+++ security-center-results/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+import logging
+from azure.identity import ClientSecretCredential
+from azure.mgmt.security import SecurityCenter
+from azure.mgmt.subscription import SubscriptionClient
+from collections import Counter
+
+client_id = os.environ["ARM_CLIENT_ID"]
+secret = os.environ["ARM_CLIENT_SECRET"]
+tenant = os.environ["TENANT_ID"]
+
+print("here")
+
+CREDENTIALS = ClientSecretCredential(
+    client_id=client_id, client_secret=secret, tenant_id=tenant,
+)
+
+client = SubscriptionClient(CREDENTIALS)
+
+compliance = []
+alerts = []
+
+
+for sub in client.subscriptions.list():
+    sub_id = f"/subscriptions/{sub.subscription_id}"
+
+    if sub.state == "Enabled":
+        try:
+            client = SecurityCenter(
+                CREDENTIALS, sub.subscription_id, asc_location="westeurope"
+            )
+            for al in client.alerts.list():
+                alerts.append(f"{al.severity}-{al.alert_display_name}")
+
+            for assessment in client.assessments.list(scope=sub_id):
+                if assessment.status.code == "Unhealthy":
+                    compliance.append(assessment.display_name)
+        except Exception as e:
+            logging.error(f"Unable to get data - {sub_id}. {e}")
+print("Alert results:")
+for i in Counter(alerts).most_common():
+    print(i)
+
+print("Compliance results:")
+for i in Counter(compliance).most_common():
+    print(i)
